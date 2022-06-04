1253a7a17fd10887e13d1cbd8355cdc90401ceab
anggiyop <anggiyohanespdd@gmail.com>
2022-06-04 20:14:13 +0700

    anggi punya nich

---
+++ anggi/koneksi.php
@@ -0,0 +1,10 @@
+<!-- koneksi.php untuk setting db -->
+
+<?php
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db = "biodata_aktor_aktris";
+
+$mysqli = new mysqli($host, $user, $pass, $db);
+?>
